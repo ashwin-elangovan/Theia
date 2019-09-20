efcaa8c2878ebded91a88feae5dacc76a22fcf17
ash-elangovan <ashelangovan97@gmail.com>
2019-09-20 10:01:32 +0530

    Final commit for v0.0.1
    Signed-off-by: ash-elangovan <ashelangovan97@gmail.com>

---
+++ lib/theia.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'theia/version'
+require 'theia/utils'
+require 'theia/screenshot'
+require 'theia/image_compare'
+require 'theia/image_compress'
+
+require 'schmooze'
---
+++ lib/theia/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Theia
+  VERSION = '0.0.1'.freeze
+end
