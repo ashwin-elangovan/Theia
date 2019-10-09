457f36e466e3174e2aa8b506f1c5bc6901f3f19b
ash-elangovan <ashelangovan97@gmail.com>
2019-10-09 17:32:56 +0530

    Added spec for screenshot
    Signed-off-by: ash-elangovan <ashelangovan97@gmail.com>

---
+++ spec/theia_spec/screenshot_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Theia::Screenshot do
+  let(:theia) { described_class.new(website_url) }
+  let(:website_url) { 'http://example.com' }
+  let(:device) { 'Kindle Fire HDX' }
+  let(:options) { {} }
+
+  describe '.new' do
+    subject(:new) { described_class.new('http://example.com') }
+    it { expect(new.instance_variable_get('@url')).to eq 'http://example.com' }
+	end
+
+  describe '#to_png' do
+    subject(:to_png) { theia.to_png("ex.png") }
+    let(:image) { MiniMagick::Image.read to_png }
+
+    it { expect(to_png.unpack('C*')).to start_with "\x89PNG\r\n\x1A\n".unpack('C*') }
+    it { expect(image.type).to eq 'PNG' }
+    it { expect(image.dimensions).to eq [1600, 2560] }
+    # To be added after html support
+    # it { expect(mean_colour_statistics(image)).to eq %w[0 128 0] }
+  end
+
+  describe '#to_jpeg' do
+    subject(:to_jpeg) { theia.to_jpeg("ex.jpeg") }
+
+    let(:image) { MiniMagick::Image.read to_jpeg }
+
+    it { expect(to_jpeg.unpack('C*')).to start_with [0xFF, 0xD8, 0xFF] }
+    it { expect(to_jpeg[6..9]).to eq 'JFIF' }
+    it { expect(to_jpeg.unpack('C*')).to end_with [0xFF, 0xD9] }
+    it { expect(image.type).to eq 'JPEG' }
+    it { expect(image.dimensions).to eq [1600, 2560] }
+    # it { expect(mean_colour_statistics(image)).to eq %w[129 0 127] }
+  end
+
+  def mean_colour_statistics(image)
+    %w[red green blue].map { |colour| image.data.dig('channelStatistics', colour, 'mean') }
+  end
+end
